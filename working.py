2ceadc0c4bc860b51ba7121dc2e3a4791c3ed002
Khoa Doan <dntkhoa@gmail.com>
2020-08-14 19:45:27 +0200

    Define a function to find the empty position in the board

--- working.py
+++ working.py
@@ -26,4 +26,8 @@ def print_board(bo):
 
 print_board(board)
 
-
+def find_empty(bo):
+    for i in range(len(bo)):
+        for j in range(len(bo[0])):
+            if bo[i][j] == 0:
+                return (i, j)
